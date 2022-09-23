f1e79761d70d5b164d20348c65cc28bf3717a49b
AldricVS <aldric.silvestre@outlook.fr>
2022-09-23 13:14:19 +0200

    Fix connection error

--- src/main/java/fr/m2_cyu_indexation/engine/persistence/oracle/OracleConnectionHandler.java
+++ src/main/java/fr/m2_cyu_indexation/engine/persistence/oracle/OracleConnectionHandler.java
@@ -18,24 +18,48 @@ import java.sql.Statement;
  */
 public class OracleConnectionHandler implements AutoCloseable {
 
+    private final String username;
+    private final String password;
+    private final String ip;
+    private final int port;
+    private final String sid;
+
     private OracleConnection connection;
 
     public OracleConnectionHandler(String username, String password, String ip, int port, String sid) throws SQLException {
-        System.out.println("Create connection to oracledb");
+        this.username = username;
+        this.password = password;
+        this.ip = ip;
+        this.port = port;
+        this.sid = sid;
         // Oracle driver implementation always returns a OracleConnection instance,
         // but no way to avoid the cast here
-        connection = (OracleConnection) DriverManager.getConnection(
+        connection = createConnection(this.username, this.password, this.ip, this.port, this.sid);
+    }
+
+    private OracleConnection createConnection(String username, String password, String ip, int port, String sid)
+            throws SQLException {
+        System.out.println("Create connection to oracledb");
+        return (OracleConnection) DriverManager.getConnection(
                 createUrl(ip, port, sid),
                 username,
                 password
         );
     }
 
+    public void updateConnection() throws SQLException {
+        if (connection.isClosed()) {
+            connection = createConnection(username, password, ip, port, sid);
+        }
+    }
+
     public Statement createStatement() throws SQLException {
+        updateConnection();
         return connection.createStatement();
     }
 
     public PreparedStatement createPreparedStatement(String query) throws SQLException {
+        updateConnection();
         return connection.prepareStatement(query);
     }
 
@@ -56,6 +80,8 @@ public class OracleConnectionHandler implements AutoCloseable {
     @Override
     public void close() throws Exception {
         System.out.println("Close oracledb connection");
-        connection.close();
+        if (!connection.isClosed()){
+            connection.close();
+        }
     }
 }
--- src/main/java/fr/m2_cyu_indexation/ui/sub_panels/results/ImageDownloaderThread.java
+++ src/main/java/fr/m2_cyu_indexation/ui/sub_panels/results/ImageDownloaderThread.java
@@ -18,25 +18,32 @@ public class ImageDownloaderThread extends Thread {
 
     @Override
     public void run() {
-        Engine engine = gridPanel.getContext().getEngine();
-        List<ImageResponse> imageResponses = engine.getResponses();
-        System.out.println("Start downloading images");
-        for (var imageResponse : imageResponses) {
-            if (this.isInterrupted()) {
-                System.out.println("Stop downloader thread");
-                break;
+        try {
+            Engine engine = gridPanel.getContext().getEngine();
+            List<ImageResponse> imageResponses = engine.getResponses();
+            System.out.println("Start downloading images");
+            for (var imageResponse : imageResponses) {
+                if (this.isInterrupted()) {
+                    System.out.println("Stop downloader thread");
+                    break;
+                }
+                String imageName = imageResponse.getImageName();
+                System.out.println("Download " + imageName);
+                var data = engine.downloadImageData(imageName);
+                if (data.length == 0) {
+                    System.out.println("Error when downloading the image");
+                    break;
+                }
+                imageResponse.setData(data);
+                gridPanel.updateGrid();
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    break;
+                }
             }
-            String imageName = imageResponse.getImageName();
-            System.out.println("Download " + imageName);
-            var data = engine.downloadImageData(imageName);
-            imageResponse.setData(data);
-            gridPanel.updateGrid();
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                break;
-            }
+            System.out.println("End of the downloader thread");
+        } catch (Exception e) {
         }
-        System.out.println("End of the downloader thread");
     }
 }
